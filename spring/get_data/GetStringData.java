1e92061219f8e6c943d1a0575b57cf5283ebf826
hwhyeons <86088505+hwhyeons@users.noreply.github.com>
2023-07-03 17:27:32 +0900

    Create GetStringData.java

---
+++ spring/get_data/GetStringData.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+특정 주소 URL을 실행해서 문자열 데이터를 받아오는 코드
+*/
+public class GetDataFromSpring {
+    public static void main(String[] args) {
+        try {
+            // Create a URL object
+            URL url = new URL("http://localhost:8080/getWorkByDate?date=2023-07-03");
+
+            // Open a connection to the URL
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+            // Set the request method
+            connection.setRequestMethod("GET");
+
+            // Get the response code
+            int responseCode = connection.getResponseCode();
+            System.out.println("Response Code: " + responseCode);
+
+            // Read the response
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String line;
+            StringBuilder response = new StringBuilder();
+            while ((line = reader.readLine()) != null) {
+                response.append(line);
+            }
+            reader.close();
+
+            // Print the response
+            System.out.println("Response: " + response.toString());
+
+            // Close the connection
+            connection.disconnect();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
