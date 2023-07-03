8445bb1b116c833b3445388167fcc647b45c1c9a
hwhyeons <86088505+hwhyeons@users.noreply.github.com>
2023-07-03 17:43:28 +0900

    Update Server_Controller.java

--- spring/get_data/Server_Controller.java
+++ spring/get_data/Server_Controller.java
@@ -1,6 +1,7 @@
     @RequestMapping(value = "/getWorkByDate",method=GET)
     @ResponseBody
     public String getWorkByDate(@RequestParam String date) {
+        // 리턴 타입을 String이 아니라 Work와 같이 객체로 변환 돼서 리턴 됨
         // List<Work> allByDate = workRepository.findAllByDate(date);
         return "tmp_test_return_value";
     }
